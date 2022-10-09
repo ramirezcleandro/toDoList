411cf1f420af491b255807f92459a0a642f15eab
Leandro <ramirezcleandro@gmail.com>
2022-10-09 09:12:18 -0400

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Leandro on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
